3cbcda21ea6ec993e4bb68f960b1495cbe189192
Michał Melka <michal.melka1@gmail.com>
2022-03-03 12:00:27 +0100

    Fix #1
    Fixed bugs with calculation.

--- index.js
+++ index.js
@@ -1,35 +1,65 @@
-const currOne = document.querySelector('#curr-one')
-const currTwo = document.querySelector('#curr-two')
-const amountOne = document.querySelector('.am1')
-const amountTwo = document.querySelector('.am2')
-const swapBtn = document.querySelector('.swap')
-const rateInfo = document.querySelector('.rate-info')
+const income = document.querySelector('.incomes>.cash')
+const exp = document.querySelector('.exp>.cash')
+const payment = document.querySelector('.money>.cash')
+const dModeBtn = document.querySelector('.dmode')
+// buttons section
+const addTransactionBtn = document.querySelector('.buttons>.add')
+const deleteAllBtn = document.querySelector('.buttons>.del')
+// popup
+const popup = document.querySelector('.popup')
+const popupAddBtn = document.querySelector('.popup>.add')
+const popupCloseBtn = document.querySelector('.popup>.del')
+const errorInfo = document.querySelector('.errorInfo')
+// history section
+const history = document.querySelector('.history')
+const money=[];
+const exps = [];
 
-const API_KEY = 'a9ee0b32938a467e19442f28'
-const API_LINK = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/PLN`
-
-const calculate = () =>{
-    fetch(API_LINK)
-    .then(res=> res.json())
-    .then(data=>{
-        // currOne.value=data.base_code
-        const currency1 = currOne.value;
-        const currency2 = currTwo.value;
-
-        const rate = data.conversion_rates[currency2];
-        rateInfo.textContent=`1 ${currency1} = ${rate.toFixed(2)} ${currency2}`
-        amountTwo.value = (amountOne.value * rate).toFixed(2)
+// dark mode 
+const darkMode = () =>{
+    document.body.classList.toggle('dark-mode')
+    document.body.querySelectorAll('*').forEach(element => {
+        element.classList.toggle('dark-mode')
     })
+    if(document.body.classList.contains('dark-mode')){
+        dModeBtn.innerHTML='<i class="fa-solid fa-moon"></i>'
+    }else{
+        dModeBtn.innerHTML='<i class="fa-solid fa-sun"></i>'
+    }
 }
-const swap = () =>{
-    const temp = currOne.value
-    currOne.value = currTwo.value
-    currTwo.value = temp
-    calculate()
+// popup open and close functions
+const openPopup = () =>{
+    popup.classList.add('active')
+}
+const closePopup =() =>{
+    popup.classList.remove('active')
+}
+let res = 0
+    let expSum = 0
+const addTransaction = () =>{
+    const popupValue = document.querySelector('.popup>#val')
+    const popupCategory = document.querySelector('.popup>#category')
+    console.log(popupCategory)
+    console.log(popupValue)
+
+    if(popupCategory.value==='payment'){
+        money.push(Number(popupValue.value))
+        res = money.reduce( (a,b) => a + b)
+        income.textContent = `${res} $`
+        payment.textContent = `${res} $`
+    }
+    else{
+        exps.push(Number(popupValue.value))
+        expSum = exps.reduce((a,b)=>a+b)
+        exp.textContent = `-${expSum} $`
+        payment.textContent = `${res - expSum} $`   
+    }
+    popup.classList.remove('active')
+    
 }
 
-currOne.addEventListener('change',calculate)
-currTwo.addEventListener('change',calculate)
-amountOne.addEventListener('input',calculate)
-swapBtn.addEventListener('click',swap)
-calculate()
+
+dModeBtn.addEventListener('click',darkMode)
+addTransactionBtn.addEventListener('click',openPopup)
+popupCloseBtn.addEventListener('click', closePopup)
+popupAddBtn.addEventListener('click', addTransaction)
